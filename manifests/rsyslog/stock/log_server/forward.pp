bf192d36b465ce8721f1e85068c356b7d6c95c05
Trevor Vaughan <tvaughan@onyxpoint.com>
2015-07-31 21:23:04 -0400

    (SIMP-61) Update to the latest rsyslog module
    This adds support for the rsyslog module rewrite.
    
    SIMP-61 #close #comment Rsyslog module rewrite update.
    
    Change-Id: I663b229085f1a8492a96de6e0249c92ea1aa7d0e

--- manifests/rsyslog/stock/log_server/forward.pp
+++ manifests/rsyslog/stock/log_server/forward.pp
@@ -3,22 +3,25 @@
 # Set this appropriately if you're forwarding your syslog data to somewhere
 # else.
 #
-# Define for adding a fowarding host to the log server.
+# Define for adding fowarding hosts to the log server.
 #
-# DO NOT make forward_host the local log host, this will cause all kinds of
+# DO NOT make forward_hosts the local log host, this will cause all kinds of
 # problems with looping logs and eat your disks.
 #
-# Note: This will forward *all* logs to the named host.
+# Note: This will forward *all* logs to the named hosts.
 #
 # == Parameters
 #
-# [*forward_host*]
-#   The host to which to forward the logs.
+# [*forward_hosts*]
+#   Type: Array
+#   The hosts to which to forward the logs.
 #
 # [*forward_port*]
+#   Type: Port
 #   The port to which to forward the logs.
 #
 # [*log_transport*]
+#   Type: One of ['tcp','udp','relp']
 #   The transport to pass the data across. Make sure it stays lower case.
 #
 # == Authors
@@ -26,11 +29,12 @@
 # * Trevor Vaughan <mailto:tvaughan@onyxpoint.com>
 #
 class simp::rsyslog::stock::log_server::forward (
-  $forward_host,
+  $forward_hosts,
   $forward_port = '6514',
   $log_transport = 'tcp'
 ) {
-  validate_net_list($forward_host)
+  validate_array($forward_hosts)
+  validate_net_list($forward_hosts)
   validate_integer($forward_port)
   validate_array_member($log_transport,['tcp','udp','relp'])
 
@@ -38,7 +42,7 @@ class simp::rsyslog::stock::log_server::forward (
 
   rsyslog::rule::remote { 'all_forward':
     rule      => '*.*',
-    dest      => $forward_host,
+    dest      => $forward_hosts,
     dest_type => $log_transport
   }
 }
