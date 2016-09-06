46c3cf7430e1a911e7df5dc7afb897ff906a3983
nicholasmhughes <nicholasmhughes@gmail.com>
2016-09-06 18:52:56 -0400

    (SIMP-1446) Prevent log duplication and log where intended
    - Introduced rule orders so messages hit 'stop' rules before they
      reach the 'catch-alls'
    
    SIMP-1446 #comment done in simp
    
    Change-Id: I8a853a9214fb01b0be57873e4b4c3103f88e0a0d

--- spec/classes/rsyslog/stock_spec.rb
+++ spec/classes/rsyslog/stock_spec.rb
@@ -31,7 +31,45 @@ describe 'simp::rsyslog::stock' do
           }}
 
           it { is_expected.to compile.with_all_deps }
+
+          # simp::rsyslog::stock::log_server
           it { is_expected.to create_class('simp::rsyslog::stock::log_server') }
+          it { is_expected.to contain_rsyslog__template__string('sudosh_template') }
+          it { is_expected.to contain_rsyslog__template__string('httpd_err_template') }
+          it { is_expected.to contain_rsyslog__template__string('httpd_template') }
+          it { is_expected.to contain_rsyslog__template__string('dhcpd_template') }
+          it { is_expected.to contain_rsyslog__template__string('puppet_agent_err_template') }
+          it { is_expected.to contain_rsyslog__template__string('puppet_agent_template') }
+          it { is_expected.to contain_rsyslog__template__string('puppet_master_err_template') }
+          it { is_expected.to contain_rsyslog__template__string('puppet_master_template') }
+          it { is_expected.to contain_rsyslog__template__string('audit_template') }
+          it { is_expected.to contain_rsyslog__template__string('slapd_audit_template') }
+          it { is_expected.to contain_rsyslog__template__string('iptables_template') }
+          it { is_expected.to contain_rsyslog__template__string('secure_template') }
+          it { is_expected.to contain_rsyslog__template__string('messages_template') }
+          it { is_expected.to contain_rsyslog__template__string('maillog_template') }
+          it { is_expected.to contain_rsyslog__template__string('cron_template') }
+          it { is_expected.to contain_rsyslog__template__string('spooler_template') }
+          it { is_expected.to contain_rsyslog__template__string('boot_template') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_sudosh') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_httpd_error') }
+          it { is_expected.to contain_rsyslog__rule__local('1_default_httpd') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_dhcpd') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_puppet_agent_error') }
+          it { is_expected.to contain_rsyslog__rule__local('1_default_puppet_agent') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_puppet_master_error') }
+          it { is_expected.to contain_rsyslog__rule__local('1_default_puppet_master') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_audit') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_slapd_audit') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_kern') }
+          it { is_expected.to contain_rsyslog__rule__local('7_default_security_relevant_logs') }
+          it { is_expected.to contain_rsyslog__rule__local('9_default_message') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_mail') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_cron') }
+          it { is_expected.to contain_rsyslog__rule__console('0_default_emerg') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_spool') }
+          it { is_expected.to contain_rsyslog__rule__local('0_default_boot') }
+
           it { is_expected.to create_file('/etc/rsyslog.conf') }
           it { is_expected.to create_file('/etc/cron.hourly/logrotate') }
           it { is_expected.to create_file('/etc/cron.daily/logrotate').with_ensure('absent') }
