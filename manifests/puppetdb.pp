14b12b20ac0ed05dd6a13accf56e0f182ef6ef55
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-09-30 17:11:33 -0400

    (SIMP-1450) Updates for SIMP 6
    * Added support for the refactored 'simp_apache'
    * Updated support for 'PuppetDB'
    
    SIMP-1450 #comment Updates for SIMP 6
    
    Change-Id: If49d6ec106b8a0c79101e22a35dddb6020c6a683

--- manifests/puppetdb.pp
+++ manifests/puppetdb.pp
@@ -29,146 +29,119 @@
 #   * Trevor Vaughan <tvaughan@onyxpoint.com>
 #
 class simp::puppetdb (
-  $client_nets              = defined('$::client_nets') ? { true  => $::client_nets, default =>  hiera('client_nets',['127.0.0.1']) },
-  $listen_address           = '127.0.0.1',
-  $listen_port              = '8138',
-  $open_listen_port         = false,
-  $ssl_listen_address       = '0.0.0.0',
-  $ssl_listen_port          = '8139',
-  $disable_ssl              = false,
-  $open_ssl_listen_port     = $puppetdb::params::open_ssl_listen_port,
-  $ssl_dir                  = $puppetdb::params::ssl_dir,
-  $ssl_set_cert_paths       = $puppetdb::params::ssl_set_cert_paths,
-  $ssl_cert_path            = $puppetdb::params::ssl_cert_path,
-  $ssl_key_path             = $puppetdb::params::ssl_key_path,
-  $ssl_ca_cert_path         = $puppetdb::params::ssl_ca_cert_path,
-  $ssl_deploy_certs         = $puppetdb::params::ssl_deploy_certs,
-  $ssl_key                  = $puppetdb::params::ssl_key,
-  $ssl_cert                 = $puppetdb::params::ssl_cert,
-  $ssl_ca_cert              = $puppetdb::params::ssl_ca_cert,
-  $ssl_protocols            = $puppetdb::params::ssl_protocols,
-  $manage_dbserver          = $puppetdb::params::manage_dbserver,
-  $manage_package_repo      = false,
-  $postgres_version         = $puppetdb::params::postgres_version,
-  $database                 = $puppetdb::params::database,
-  $database_host            = $puppetdb::params::database_host,
-  $database_port            = $puppetdb::params::database_port,
-  $database_username        = $puppetdb::params::database_username,
-  $database_password        = passgen('simp_puppetdb'),
-  $database_name            = $puppetdb::params::database_name,
-  $database_ssl             = $puppetdb::params::database_ssl,
-  $database_listen_address  = $puppetdb::params::postgres_listen_addresses,
-  $database_validate        = $puppetdb::params::database_validate,
-  $database_embedded_path   = $puppetdb::params::database_embedded_path,
-  $node_ttl                 = $puppetdb::params::node_ttl,
-  $node_purge_ttl           = $puppetdb::params::node_purge_ttl,
-  $report_ttl               = $puppetdb::params::report_ttl,
-  $gc_interval              = $puppetdb::params::gc_interval,
-  $log_slow_statements      = $puppetdb::params::log_slow_statements,
-  $conn_max_age             = $puppetdb::params::conn_max_age,
-  $conn_keep_alive          = $puppetdb::params::conn_keep_alive,
-  $conn_lifetime            = $puppetdb::params::conn_lifetime,
-  $puppetdb_package         = $puppetdb::params::puppetdb_package,
-  $puppetdb_service         = $puppetdb::params::puppetdb_service,
-  $puppetdb_service_status  = $puppetdb::params::puppetdb_service_status,
-  $puppetdb_user            = $puppetdb::params::puppetdb_user,
-  $puppetdb_group           = $puppetdb::params::puppetdb_group,
-  $read_database            = $puppetdb::params::read_database,
-  $read_database_host       = $puppetdb::params::read_database_host,
-  $read_database_port       = $puppetdb::params::read_database_port,
-  $read_database_username   = 'simp_puppetdb',
-  $read_database_password   = passgen('simp_read_puppetdb'),
-  $read_database_name       = 'simp_puppetdb',
-  $read_database_ssl        = true,
-  $read_database_validate   = $puppetdb::params::read_database_validate,
-  $read_log_slow_statements = $puppetdb::params::read_log_slow_statements,
-  $read_conn_max_age        = $puppetdb::params::read_conn_max_age,
-  $read_conn_keep_alive     = $puppetdb::params::read_conn_keep_alive,
-  $read_conn_lifetime       = $puppetdb::params::read_conn_lifetime,
-  $confdir                  = $puppetdb::params::confdir,
-  $manage_firewall          = false,
-  $java_args                = [
-                                '-Djava.io.tmpdir=/var/lib/puppetdb',
-                                '-XX:-HeapDumpOnOutOfMemoryError',
-                                '-Djava.net.preferIPv4Stack=true'
-                              ],
-  $max_threads              = $puppetdb::params::max_threads,
-  $command_threads          = $puppetdb::params::command_threads,
-  $store_usage              = $puppetdb::params::store_usage,
-  $temp_usage               = $puppetdb::params::temp_usage
-) inherits ::puppetdb::params {
+  Array $client_nets             = defined('$::client_nets') ? { true  => $::client_nets, default =>  hiera('client_nets',['127.0.0.1']) },
+  String $listen_address         = '127.0.0.1',
+  Integer $listen_port           = 8138,
+  Boolean $open_listen_port      = false,
+  Boolean $ssl_deploy_certs      = true,
+  Boolean $ssl_set_cert_paths    = true,
+  String $ssl_listen_address     = '0.0.0.0',
+  Integer $ssl_listen_port       = 8139,
+  Boolean $use_puppet_ssl_certs  = true,
+  Boolean $disable_ssl           = false,
+  Boolean $manage_package_repo   = false,
+  String $database_password      = passgen('simp_puppetdb'),
+  String $read_database_username = 'simp_puppetdb',
+  String $read_database_password = passgen('simp_read_puppetdb'),
+  String $read_database_name     = 'simp_puppetdb',
+  Boolean $read_database_ssl     = true,
+  Boolean $manage_firewall       = true,
+  String $java_max_memory        = '40%',
+  String $java_start_memory      = '',
+  String $java_tmpdir            = '/opt/puppetlabs/puppet/cache/pdb_tmp',
+  Boolean $java_heapdump_on_oom  = false,
+  Boolean $java_prefer_ipv4      = true,
+  Boolean $use_iptables          = defined('$::use_iptables') ? { true  => $::use_iptables, default => hiera('use_iptables', true) }
+) {
 
   validate_net_list($client_nets)
+  validate_port($listen_port)
+  validate_port($ssl_listen_port)
+  validate_absolute_path($java_tmpdir)
 
-  include ::iptables
+  $_simp_manage_firewall = ($manage_firewall and $use_iptables)
 
-  class { '::puppetdb':
-    listen_address           => $listen_address,
-    listen_port              => $listen_port,
-    open_listen_port         => $open_listen_port,
-    ssl_listen_address       => $ssl_listen_address,
-    ssl_listen_port          => $ssl_listen_port,
-    disable_ssl              => $disable_ssl,
-    open_ssl_listen_port     => $open_ssl_listen_port,
-    ssl_dir                  => $ssl_dir,
-    ssl_set_cert_paths       => $ssl_set_cert_paths,
-    ssl_cert_path            => $ssl_cert_path,
-    ssl_key_path             => $ssl_key_path,
-    ssl_ca_cert_path         => $ssl_ca_cert_path,
-    ssl_deploy_certs         => $ssl_deploy_certs,
-    ssl_key                  => $ssl_key,
-    ssl_cert                 => $ssl_cert,
-    ssl_ca_cert              => $ssl_ca_cert,
-    ssl_protocols            => $ssl_protocols,
-    manage_dbserver          => $manage_dbserver,
-    manage_package_repo      => $manage_package_repo,
-    postgres_version         => $postgres_version,
-    database                 => $database,
-    database_host            => $database_host,
-    database_port            => $database_port,
-    database_username        => $database_username,
-    database_password        => $database_password,
-    database_name            => $database_name,
-    database_ssl             => $database_ssl,
-    database_listen_address  => $database_listen_address,
-    database_validate        => $database_validate,
-    database_embedded_path   => $database_embedded_path,
-    node_ttl                 => $node_ttl,
-    node_purge_ttl           => $node_purge_ttl,
-    report_ttl               => $report_ttl,
-    gc_interval              => $gc_interval,
-    log_slow_statements      => $log_slow_statements,
-    conn_max_age             => $conn_max_age,
-    conn_keep_alive          => $conn_keep_alive,
-    conn_lifetime            => $conn_lifetime,
-    puppetdb_package         => $puppetdb_package,
-    puppetdb_service         => $puppetdb_service,
-    puppetdb_service_status  => $puppetdb_service_status,
-    puppetdb_user            => $puppetdb_user,
-    puppetdb_group           => $puppetdb_group,
-    read_database            => $read_database,
-    read_database_host       => $read_database_host,
-    read_database_port       => $read_database_port,
-    read_database_username   => $read_database_username,
-    read_database_password   => $read_database_password,
-    read_database_name       => $read_database_name,
-    read_database_ssl        => $read_database_ssl,
-    read_database_validate   => $read_database_validate,
-    read_log_slow_statements => $read_log_slow_statements,
-    read_conn_max_age        => $read_conn_max_age,
-    read_conn_keep_alive     => $read_conn_keep_alive,
-    read_conn_lifetime       => $read_conn_lifetime,
-    confdir                  => $confdir,
-    manage_firewall          => $manage_firewall,
-    java_args                => $java_args,
-    max_threads              => $max_threads,
-    command_threads          => $command_threads,
-    store_usage              => $store_usage,
-    temp_usage               => $temp_usage
+  $_java_max_memory = inline_template('<% if @java_max_memory[-1].chr == "%" %><%= (@memorysize_mb.to_f * (@java_max_memory[0..-2].to_f/100.0)).round.to_s + "m" %><% else %><%= @java_max_memory %><% end %>')
+
+  if empty($::puppetdb::java_args) {
+    $_java_heapdump_on_oom = $java_heapdump_on_oom ? { true => '-XX:HeapDumpOnOutOfMemoryError', default => '-XX:-HeapDumpOnOutOfMemoryError' }
+
+    if empty($java_start_memory) {
+      $_java_start_memory = $_java_max_memory
+    }
+    else {
+      $_java_start_memory = $java_start_memory
+    }
+
+    $_java_args = {
+      '-Xmx'                        => $_java_max_memory,
+      '-Xms'                        => $_java_start_memory,
+      $_java_heapdump_on_oom        => '',
+      '-Djava.io.tmpdir='           => $java_tmpdir,
+      '-Djava.net.preferIPv4Stack=' => bool2str($java_prefer_ipv4)
+    }
+  }
+  else {
+    $_java_args = $::puppetdb::java_args
   }
 
-  iptables::add_tcp_stateful_listen { 'puppetdb':
-    dports      => [$::puppetdb::ssl_listen_port],
-    client_nets => $client_nets
+  file { $java_tmpdir:
+    ensure =>  'directory',
+    owner  => 'puppetdb',
+    group  => 'puppetdb',
+    mode   => '0640',
+    before => Service[$::puppetdb::puppetdb_service]
+  }
+
+  $_my_defaults = {
+    'listen_address'         => $listen_address,
+    'listen_port'            => $listen_port,
+    'open_listen_port'       => $open_listen_port,
+    'ssl_deploy_certs'       => $ssl_deploy_certs,
+    'ssl_set_cert_paths'     => $ssl_set_cert_paths,
+    'ssl_listen_address'     => $ssl_listen_address,
+    'ssl_listen_port'        => $ssl_listen_port,
+    'disable_ssl'            => $disable_ssl,
+    'manage_package_repo'    => $manage_package_repo,
+    'database_password'      => $database_password,
+    'read_database_username' => $read_database_username,
+    'read_database_password' => $read_database_password,
+    'read_database_name'     => $read_database_name,
+    'read_database_ssl'      => $read_database_ssl,
+    'manage_firewall'        => ($manage_firewall and !($_simp_manage_firewall)),
+    'java_args'              => $_java_args
+  }
+
+  class { 'puppetdb': * => $_my_defaults }
+
+  # We need to do this to make PuppetDB use the system puppet certificates
+  if $use_puppet_ssl_certs {
+    File<| title == $::puppetdb::ssl_key_path |> {
+      content   => undef,
+      show_diff => false,
+      source    => "file://${facts['puppet_settings']['main']['hostprivkey']}"
+    }
+    File<| title == $::puppetdb::ssl_cert_path |> {
+      content   => undef,
+      show_diff => false,
+      source    => "file://${facts['puppet_settings']['main']['hostcert']}"
+    }
+    File<| title == $::puppetdb::ssl_ca_cert_path |> {
+      content   => undef,
+      show_diff => false,
+      source    => "file://${facts['puppet_settings']['main']['localcacert']}"
+    }
+  }
+
+  # Don't fight with the Puppet firewall module
+  if $_simp_manage_firewall {
+    include '::iptables'
+
+    iptables::add_tcp_stateful_listen { 'puppetdb':
+      dports      => [$::puppetdb::ssl_listen_port],
+      client_nets => $client_nets,
+      # Need this for the auto-connect test script
+      before => Service[$::puppetdb::puppetdb_service]
+    }
   }
 }
