10c68fe1af0527604f74d236d787fd1465f57914
Nick Miller <nick.miller@onyxpoint.com>
2016-12-05 16:03:03 -0500

    Updated to use

--- manifests/kmod_blacklist.pp
+++ manifests/kmod_blacklist.pp
@@ -4,7 +4,7 @@
 # @param enable_defaults Boolean
 #   Enable to use the default blacklist, otherwise just the custom_blacklist will be used.
 #
-# @param default_blacklist Array[String]
+# @param blacklist Array[String]
 #   List of kernel modules to be included by default
 #
 # @param custom_blacklist Array[String]
@@ -12,7 +12,7 @@
 #
 class simp::kmod_blacklist (
   Boolean $enable_defaults = true,
-  Array[String] $default_blacklist = [
+  Array[String] $blacklist = [
     'bluetooth',
     'cramfs',
     'dccp',
@@ -35,13 +35,13 @@ class simp::kmod_blacklist (
 ) {
 
   if $enable_defaults {
-    $blacklist = $custom_blacklist + $default_blacklist
+    $_blacklist = $custom_blacklist + $blacklist
   }
   else {
-    $blacklist = $custom_blacklist
+    $_blacklist = $custom_blacklist
   }
 
-  $blacklist.each |String $mod| {
+  $_blacklist.each |String $mod| {
     kmod::blacklist { $mod: }
   }
 
