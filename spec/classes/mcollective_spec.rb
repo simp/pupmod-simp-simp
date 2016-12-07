faa7d34b8577a36ee7516d1aeb465398e1fbda50
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-12-07 13:27:31 -0500

    Fixed ALL THE THINGS
    * Added Typing for the variables
    * Allow users to set the directories that will be managed via rsync

---
+++ spec/classes/mcollective_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'simp::mcollective' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) {
+        facts[:puppetversion] = %x{puppet --version}.strip
+
+        facts
+      }
+
+      context "on #{os}" do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('simp::mcollective') }
+        it { is_expected.to create_class('mcollective') }
+        it { is_expected.to create_class('activemq') }
+      end
+    end
+  end
+end
