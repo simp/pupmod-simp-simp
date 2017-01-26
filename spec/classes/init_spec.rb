732c6bd934d4d8b14c26459a7d7b56f3df248c85
Nick Miller <nick.miller@onyxpoint.com>
2017-01-26 10:18:19 -0500

    (SIMP-2541) Enable $rsync_stunnel by default (#81)
    SIMP-2541 #close

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -13,15 +13,19 @@ describe 'simp' do
           facts
         end
 
-        context 'default' do
-          # it { require 'pry';binding.pry }
+        context 'with default parameters' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_file('/opt/puppetlabs/puppet/cache/simp') }
           it { is_expected.to create_host('puppet.bar.baz').with_ip('1.2.3.4') }
+          it { is_expected.to create_stunnel__connection('rsync') }
         end
 
         context 'rsync_stunnel logic' do
-          context 'with rsync_stunnel defined' do
+          context 'with rsync_stunnel => false' do
+            let(:params) {{ :rsync_stunnel => false }}
+            it { is_expected.not_to create_stunnel__connection('rsync') }
+          end
+          context 'with rsync_stunnel => Simplib::Host' do
             let(:params) {{ :rsync_stunnel => 'other.test.host' }}
             it { is_expected.to create_stunnel__connection('rsync').with({
               :connect => ['other.test.host:8730'],
