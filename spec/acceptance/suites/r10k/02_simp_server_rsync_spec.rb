5a4579e8227bc56aec235cf516ec967d905fd59f
Nick Miller <nick.miller@onyxpoint.com>
2017-03-10 08:58:09 -0500

    (SIMP-2839) Add test for installing via r10k
    Add basic acceptance test for installing SIMP from r10k and a
    Puppetfile. It should set all required settings from simp_options and
    other modules.
    
    First, it uses the copied modules from beaker to set up a puppetserver
    with SIMP's pupmod module. It then copies the provided Puppetfile to the
    master, installs the r10k gem, and runs r10k puppetfile install. It
    drops in a barebones site.pp with a few node definitions and then runs
    puppet on the master and the agent.
    
    SIMP-2839 #close

---
+++ spec/acceptance/suites/r10k/02_simp_server_rsync_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper_acceptance'
+
+test_name 'simp::server::rsync_shares'
+
+describe 'install rsync from GitHub (not rpm) and test simp::server::rsync_shares' do
+  pending('TODO')
+end
---
+++ spec/acceptance/suites/r10k/03_simp_server_ldap_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper_acceptance'
+
+test_name 'simp::server::ldap'
+
+describe 'use the simp::server::ldap class to create and ldap environment' do
+  pending('TODO')
+end
