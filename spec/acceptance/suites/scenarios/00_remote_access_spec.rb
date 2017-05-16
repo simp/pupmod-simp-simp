26c59db159ed4f7e452dd716e496e61291fb2816
nmarkowski <nmarkowski@keywcorp.com>
2017-05-16 17:31:20 -0400

    Acceptance SAFETY SAVE PT II - DO NOT MERGE

--- spec/acceptance/suites/scenarios/00_remote_access_spec.rb
+++ spec/acceptance/suites/scenarios/00_remote_access_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper_acceptance'
 require 'erb'
 
-test_name 'simp class'
+test_name 'remote_access scenario'
 
-describe 'simp class' do
+# SIMP remote_access scenario acceptance
+#
+# The remote_access scenario includes the components required for
+# for a user to 'log into' a client (and nothing more!).  The test
+# procedure is as follows:
+#   1. Create an ldap server on server-el7
+#   2. Add an LDAP 'test.user'
+#   3. Apply the remote_access scenario on client-el7 and client-el6
+#   4. Attempt to authenticate as 'test.user' on client-el7 and
+#      client-el6.
+
+describe 'remote_access scenario' do
   before(:context) do
     hosts.each do |host|
       interfaces = fact_on(host, 'interfaces').strip.split(',')
@@ -31,49 +42,66 @@ describe 'simp class' do
     EOS
   }
 
-  ldap_servers = hosts_with_role(hosts, 'ldap_server')
-  #client = hosts_with_role(hosts, 'client')
+  ldap_server = only_host_with_role(hosts, 'ldap_server')
+  clients = hosts_with_role(hosts, 'client')
 
-  ldap_servers.each do |ldap_server|
-    context "test authentication using remote_access scenario" do
-      let(:server_fqdn) { fact_on(ldap_server, 'fqdn') }
-      let(:base_dn) { fact_on(ldap_server, 'domain').split('.').map{ |d| "dc=#{d}" }.join(',') }
-      let(:server_hieradata)      { File.read(File.expand_path('templates/server_hieradata.yaml.erb', File.dirname(__FILE__))) }
-      let(:add_testuser)          { File.read(File.expand_path('templates/add_testuser.ldif.erb', File.dirname(__FILE__))) }
-      let(:add_testuser_to_admin) { File.read(File.expand_path('templates/add_testuser_to_admin.ldif.erb', File.dirname(__FILE__))) }
+  # Both client and server need these
+  let(:server_fqdn) { fact_on(ldap_server, 'fqdn') }
+  let(:base_dn) { fact_on(ldap_server, 'domain').split('.').map{ |d| "dc=#{d}" }.join(',') }
 
-      context "set up simp_openldap::server on #{ldap_server}" do
-        # Required for simp ldap password policy
-        it 'should set up needed repositories' do
-          on host, 'curl -s https://packagecloud.io/install/repositories/simp-project/6_X_Dependencies/script.rpm.sh | bash'
-        end
-        it 'should configure hiera' do
-          set_hieradata_on(ldap_server, ERB.new(server_hieradata).result(binding))
-        end
-        it 'should set up an ldap server' do
-          apply_manifest_on(ldap_server, server_manifest, :catch_failures => true)
-          apply_manifest_on(ldap_server, server_manifest, :acceptable_exit_codes => [0,2])
-        end
-        it 'should be able to connect and use ldapsearch' do
-          on(ldap_server, "ldapsearch -Z -LLL -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r!")
-        end
-        it 'should be able to add a user' do
-          create_remote_file(ldap_server, '/tmp/add_testuser.ldif', ERB.new(add_testuser).result(binding))
+  context "set up simp_openldap::server on #{ldap_server}" do
+    let(:server_hieradata)      { File.read(File.expand_path('templates/server_hieradata.yaml.erb', File.dirname(__FILE__))) }
+    let(:add_testuser)          { File.read(File.expand_path('templates/add_testuser.ldif.erb', File.dirname(__FILE__))) }
+    let(:add_testuser_to_admin) { File.read(File.expand_path('templates/add_testuser_to_admin.ldif.erb', File.dirname(__FILE__))) }
 
-          on(ldap_server, "ldapadd -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x -f /tmp/add_testuser.ldif")
+    # Required for simp ldap password policy
+    it 'should set up needed repositories' do
+      on(ldap_server, 'curl -s https://packagecloud.io/install/repositories/simp-project/6_X_Dependencies/script.rpm.sh | bash')
+    end
+    it 'should configure hiera' do
+       set_hieradata_on(ldap_server, ERB.new(server_hieradata).result(binding))
+    end
+    it 'should set up an ldap server' do
+      apply_manifest_on(ldap_server, server_manifest, :catch_failures => true)
+      apply_manifest_on(ldap_server, server_manifest, :acceptable_exit_codes => [0,2])
+    end
+    it 'should be able to connect and use ldapsearch' do
+      on(ldap_server, "ldapsearch -Z -LLL -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r!")
+    end
+    it 'should be able to add a user' do
+      create_remote_file(ldap_server, '/tmp/add_testuser.ldif', ERB.new(add_testuser).result(binding))
 
-          result = on(ldap_server, "ldapsearch -Z -LLL -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x uid=test.user")
-          expect(result.stdout).to include("dn: uid=test.user,ou=People,#{base_dn}")
-        end
-        it 'should be able to add user to group' do
-          create_remote_file(ldap_server, '/tmp/add_testuser_to_admin.ldif', ERB.new(add_testuser_to_admin).result(binding))
+      on(ldap_server, "ldapadd -Z -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x -f /tmp/add_testuser.ldif")
 
-          on(ldap_server, "ldapmodify -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x -f /tmp/add_testuser_to_admin.ldif")
+      result = on(ldap_server, "ldapsearch -Z -LLL -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x uid=test.user")
+      expect(result.stdout).to include("dn: uid=test.user,ou=People,#{base_dn}")
+    end
+    it 'should be able to add user to group' do
+      create_remote_file(ldap_server, '/tmp/add_testuser_to_admin.ldif', ERB.new(add_testuser_to_admin).result(binding))
 
-          result = on(ldap_server, "ldapsearch -Z -LLL -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x cn=test.user")
-          expect(result.stdout).to include("dn: cn=test.user,ou=Group,#{base_dn}")
-        end
+      on(ldap_server, "ldapmodify -Z -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x -f /tmp/add_testuser_to_admin.ldif")
+
+      result = on(ldap_server, "ldapsearch -Z -LLL -D cn=LDAPAdmin,ou=People,#{base_dn} -H ldap://#{server_fqdn} -w suP3rP@ssw0r! -x cn=test.user")
+      expect(result.stdout).to include("dn: cn=test.user,ou=Group,#{base_dn}")
+    end
+  end
+
+  clients.each do |client|
+    context "set up remote_access scenario on #{client}" do
+      let(:client_hieradata)      { File.read(File.expand_path('templates/client_hieradata.yaml.erb', File.dirname(__FILE__))) }
+      let(:client_fqdn) { fact_on(client, 'fqdn') }
+      # Need this for sudosh2
+      it 'should set up needed repositories' do
+        on(ldap_server, 'curl -s https://packagecloud.io/install/repositories/simp-project/6_X_Dependencies/script.rpm.sh | bash')
+      end
+      it 'should configure hiera' do
+        set_hieradata_on(client, ERB.new(client_hieradata).result(binding))
+      end
+      it 'should apply the remote_access scenario without error' do
+        apply_manifest_on(client, client_manifest, :catch_failures => true)
+        apply_manifest_on(client, client_manifest, :catch_failures => true)
       end
     end
   end
+
 end
