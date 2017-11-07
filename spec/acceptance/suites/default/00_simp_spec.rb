0b63ed23a5864e36ba18450ba75fa50a8b8bd374
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2017-11-07 13:47:06 -0500

    (SIMP-3969) pupmod: Acceptance test fixes for FIPS (#131)
    Fixes to allow acceptance tests to run on servers in FIPS mode.
    
    SIMP-3969 #comment pupmod acceptance test fixes for FIPS

--- spec/acceptance/suites/default/00_simp_spec.rb
+++ spec/acceptance/suites/default/00_simp_spec.rb
@@ -57,7 +57,7 @@ useradd::securetty:
       # These boxes have no root password by default...
       it 'should set the root password' do
         on(host, "sed -i 's/enforce_for_root//g' /etc/pam.d/*")
-        on(host, 'echo "root:password" | chpasswd')
+        on(host, 'echo "root:password" | chpasswd --crypt-method SHA256')
       end
 
       it 'should set up needed repositories' do
