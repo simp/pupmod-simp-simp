8aa610f5a6a4db34d36ff1054c8801cc60017b8e
lnemsick-simp <lnemsick.simp@gmail.com>
2018-05-07 17:12:43 -0400

    (SIMP-4860) Create systemd/sysv runpuppet scripts
    runpuppet changes
    - Created systemd service file that allows the appropriate systemd timeout
      mechanisms to be applied.  Previously, the sysv script was being
      killed by systemd, when it ran longer than the default timeout for a
      service start.  Now, the timeout for runpuppet can be set appropriately
      for a site's needs.
    - Used bootstrap_simp_client to provide the bulk of the bootstrap
      processing in both versions of runpuppet.  The benefits of this
      script include:
      - More effective puppet agent processing.  The bootstrapping
        takes fewer puppet agent runs.
      - Finer grained control of the bootstrap algorithm.
      - Exponential backoff of requests to the Puppet server, to minimize
        Puppet server overload.
      - Error handling
        - Bootstrap operation errors are now detected and logged.
        - Failed puppet agent runs are now retried, instead of blindly
          continuing on.
      - Timestamped log messages in the bootstrap log file.  This includes
        messages from bootstrap_simp_client, puppet agent, and fixfiles.
    - Added installation of bootstrap_simp_client in the same location
      as runpuppet.
    - Added parameters for most of the (numerous) bootstrap_simp_client
      configuration parameters.
    - Added the option to force a client reboot on client bootstrap failure.
    - Removed OBE logic for bootstrapping a SIMP server
    
    bootstrap_simp_client changes
    - Moved most of the handling of the bootstrap lock from runpuppet into
      bootstrap_simp_client, to simplify the runpuppet service files.
    - Added the logic to disable the runpuppet service and enable the
      the puppet service, to simplify the runpuppet service files.
    - Added an option to set static hostname via hostnamectl, to
      simplify the systemd runpuppet service file.
    
    NOTE:
    - Use of this capability requires changes to the pupclient_x86_64.cfg
      files (SIMP-4877):
      - CentOS 6:
        - Need to add logic to download bootstrap_simp_client into
          /usr/local/sbin and give it 700 permissions
      - CentOS 7:
        - Need to add logic to download runpuppet into /etc/systemd/system,
          name it 'runpuppet.service', and give it 644 permissions
        - Needs to add logic to download bootstrap_simp_client into
          /usr/local/sbin and give it 700 permissions
        - Needs to replace the runpuppet chkconfig logic with
          'systemctl enable runpuppet.service'
    - Although runpuppet.service should ideally be a oneshot service,
      when run as a oneshot, it aborts when bootstrap_simp_client calls
      hostnamectl.
    - The ExecStart in runpuppet.service is ugly.  We could wrap that
      in yet another script, but that would require the client to download
      another file from the KS server.
    - systemd sends the runpuppet.service stdout/stderr console messages to
      syslog with the 'sh' identity.  Doesn't seem like there is an easy fix for
      this. However, if this is too confusing to the user, we can suppress
      these messages with the --quiet option of bootstrap_simp_client.
      (These messages already are logged with the correct identity to the
      bootstrap log.)
    - The CentOS 6 service script has some LOCKED logic in it because of
      the status command.  This is a bit ugly, but is not worth fixing for
      the old OS.
    
    SIMP-4860 #close

--- manifests/server/kickstart/runpuppet.pp
+++ manifests/server/kickstart/runpuppet.pp
@@ -5,13 +5,16 @@
 # @param location The location of the runpuppet file to be placed when
 #   generated.
 #
+#   *  The bootstrap_simp_client script used by runpuppet will
+#      be placed in the same directory of the runpuppet file.
+#
 # @param ntp_servers
 #   An array of ntp servers or hash of server/value pairs that should
 #   be used during client kickstarts to slew the local time correctly
 #   prior to PKI key distribution.
 #
-#   Failure to set the system clock will not cause the runpuppet script to fail
-#   to execute.
+#   **NOTE**: Failure to set the system clock will not cause the runpuppet
+#   script to fail to execute.
 #
 # @param puppet_server
 #   The FQDN of your Puppet server
@@ -39,6 +42,48 @@
 #   If set to false or 0, the client will immediately timeout if a signed
 #   certificate is not presented.
 #
+# @param puppet_digest_algorithm
+#   The digest algorithm Puppet uses for file resources and the filebucket
+#   (e.g. sha256, sha384, sha512).
+#
+# @param puppet_keylength
+#   Puppet certificate keylength.  When unset, value is determined based
+#   on `$fips`, to work around Puppet bugs in FIPS mode.  (See `$fips`.)
+#
+# @param num_puppet_runs
+#   Number of puppet agent runs (after the initial tagged run) to execute,
+#   in order to converge to a stable system configuration.
+#
+# @param initial_retry_interval
+#   Initial retry interval in seconds for reattempting a failed puppet
+#   agent run.
+#
+# @param retry_factor
+#   The factor to be applied to the retry interval for a puppet run.
+#   The retry interval is multiplied by this factor for each retry.
+#   For example, if `$initial_retry_interval` is 10 and the retry factor
+#   is 1.5, the first retry would occur 10 seconds after the initial
+#   attempt, the second retry would occur 10*1.5 seconds after that,
+#   the third retry would occur 10*1.5*1.5 seconds after that, etc.
+#
+# @param max_seconds
+#   Maximum number of seconds this bootstrap script is allowed to run.
+#   Script will abort if it does not complete within this allotted time.
+#
+# @param reboot_on_failure
+#   Whether to reboot the server if runpuppet fails to bootstrap the
+#   client.  This allows the client to attempt fix its bootstrap problem
+#   without manual intervention.  However, for sites containing a large
+#   number of clients, the repeated cycle of <multiple puppet agent
+#   attempts + reboot> may overtax the Puppet server.  In this case,
+#   disabling this feature may be most appropriate.
+#
+# @param set_static_hostname
+#   Whether to persist the hostname retrieved by DHCP as a static
+#   hostname.  This prevents problems that can arise when the DHCP
+#   lease expires in the middle of bootstrap puppet runs.  Is not
+#   applicable for RedHat/CentOS 6.
+#
 # @param fips
 #   If true, set puppet keylength to 2048, else 4096.  This non-compliant
 #   setting is to work around problems with older versions of Ruby.  It
@@ -51,8 +96,16 @@ class simp::server::kickstart::runpuppet (
   Optional[Simplib::Host]     $puppet_ca               = simplib::lookup('simp_options::puppet::ca', { 'default_value' => undef }),
   Simplib::Port               $puppet_ca_port          = simplib::lookup('simp_options::puppet::ca_port', { 'default_value' => 8141 }),
   Stdlib::Absolutepath        $location                = '/var/www/ks/runpuppet',
+  Boolean                     $set_static_hostname     = true,
+  Boolean                     $reboot_on_failure       = true,
   Boolean                     $runpuppet_print_stats   = true,
-  Variant[Integer[0],Boolean] $runpuppet_wait_for_cert = 10
+  Variant[Integer[0],Boolean] $runpuppet_wait_for_cert = 10,
+  Integer[1]                  $num_puppet_runs         = 2,
+  Integer[1]                  $initial_retry_interval  = 10,
+  Float[0.1]                  $retry_factor            = 1.5,
+  Integer[1]                  $max_seconds             = 1800,
+  String                      $puppet_digest_algorithm = 'sha256',
+  Optional[Integer[2048]]     $puppet_keylength        = undef,
 ) {
 
   simplib::assert_metadata( $module_name )
@@ -77,12 +130,37 @@ class simp::server::kickstart::runpuppet (
     $_puppet_ca = $facts['puppet_settings']['agent']['ca_server']
   }
 
+  if $puppet_keylength {
+    $_puppet_keylength = $puppet_keylength
+  } elsif $fips {
+    $_puppet_keylength = 2048
+  } else {
+    $_puppet_keylength = 4096
+  }
+
+
+  # This is the bootstrap helper script used by both the systemd and systemv
+  # versions of runpuppet.
+  $_dir = dirname($location)
+  file { "${_dir}/bootstrap_simp_client":
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'apache',
+    mode    => '0640',
+    content => file("${module_name}/var/www/ks/bootstrap_simp_client")
+  }
+
+  if member($facts['init_systems'], 'systemd') {
+    $_template = "${module_name}/www/ks/runpuppet_systemd.erb"
+  } else {
+    $_template = "${module_name}/www/ks/runpuppet.erb"
+  }
+
   file { $location:
     ensure  => 'file',
     owner   => 'root',
     group   => 'apache',
     mode    => '0640',
-    content => template("${module_name}/www/ks/runpuppet.erb")
+    content => template($_template)
   }
-
 }
--- spec/classes/server/kickstart/runpuppet_spec.rb
+++ spec/classes/server/kickstart/runpuppet_spec.rb
@@ -1,73 +1,121 @@
 require 'spec_helper'
 
 describe 'simp::server::kickstart::runpuppet' do
-  context 'supported operating systems' do
-    on_supported_os.each do |os, facts|
-      context "on #{os}" do
-        let(:facts) do
-          facts[:servername] = 'my.happy.server'
-          facts[:server_facts] = { :servername => 'my.happy.server' }
-          facts
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts = os_facts.dup
+        facts[:servername] = 'my.happy.server'
+        facts[:server_facts] = { :servername => 'my.happy.server' }
+        facts
+      end
+
+      let(:init_sys_suffix) do
+        if (os_facts[:init_systems].include?('systemd'))
+           '_systemd'
+        else
+           ''
+        end
+      end
+
+      context 'default parameters (using fixtures/hieradata/default.yaml)' do
+        it { is_expected.to compile.with_all_deps }
+        it do
+           expected_content = File.read(File.join(File.dirname(__FILE__),
+             '..', '..', '..', '..', 'files', 'var', 'www', 'ks',
+             'bootstrap_simp_client'))
+
+          is_expected.to create_file('/var/www/ks/bootstrap_simp_client').with({
+            :ensure  => 'file',
+            :owner   => 'root',
+            :group   => 'apache',
+            :mode    => '0640',
+            :content => expected_content
+          })
         end
 
-        context 'default parameters (using fixtures/hieradata/default.yaml)' do
-          it { is_expected.to compile.with_all_deps }
-          it {
+        it do
           expected_content = File.read(File.join(File.dirname(__FILE__), 'files',
-            'default_runpuppet'))
-            is_expected.to create_file('/var/www/ks/runpuppet').with_content(expected_content)
+            "default_runpuppet#{init_sys_suffix}"))
+          is_expected.to create_file('/var/www/ks/runpuppet').with_content(expected_content)
+        end
+      end
+
+      context 'with reboot_on_failure=false' do
+        let(:params) {{
+          :reboot_on_failure => false
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          expected_content = File.read(File.join(File.dirname(__FILE__), 'files',
+            "runpuppet_without_reboot_on_failure#{init_sys_suffix}"))
+          is_expected.to create_file('/var/www/ks/runpuppet').with_content(expected_content)
+        end
+      end
+
+      context 'with fips=true' do
+        let(:params) {{
+          :fips => true
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_file('/var/www/ks/runpuppet').with_content(/--puppet-keylength 2048/) }
+      end
+
+      context 'ntp_servers array' do
+        let(:params) {{
+          :ntp_servers => ['1.2.3.4','5.6.7.8']
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          expected_content = File.read(File.join(File.dirname(__FILE__), 'files',
+            "runpuppet_with_ntp_servers#{init_sys_suffix}"))
+          is_expected.to create_file('/var/www/ks/runpuppet').with_content(expected_content)
+        end
+      end
+
+      context 'ntp_servers hash' do
+        let (:params) {{
+          :ntp_servers => {
+            '1.2.3.4' => ['foo, bar'],
+            '5.6.7.8' => ['baz']
           }
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+        it do
+          expected_content = File.read(File.join(File.dirname(__FILE__), 'files',
+            "runpuppet_with_ntp_servers#{init_sys_suffix}"))
+          is_expected.to create_file('/var/www/ks/runpuppet').with_content(expected_content)
         end
+      end
 
-        context 'with fips=true' do
-          let(:params) {{
-            :fips => true
-          }}
+      context 'print_stats=false' do
+        let(:params) {{
+          :runpuppet_print_stats => false
+        }}
 
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_file('/var/www/ks/runpuppet').with_content(/keylength         = 2048/) }
+        it { is_expected.to compile.with_all_deps }
+        it do
+          expected_content = File.read(File.join(File.dirname(__FILE__), 'files',
+            "runpuppet_without_print_stats#{init_sys_suffix}"))
+          is_expected.to create_file('/var/www/ks/runpuppet').with_content(expected_content)
         end
+      end
 
-        context 'specify_ntp_servers_array' do
-          let(:params) {{
-            :ntp_servers => ['1.2.3.4','5.6.7.8']
-          }}
+      context 'wait_for_cert=false' do
+        let(:params) {{
+          :runpuppet_wait_for_cert => false
+        }}
 
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_file('/var/www/ks/runpuppet').with_content(/ntpdate -b 1.2.3.4 5.6.7.8/) }
+        it { is_expected.to compile.with_all_deps }
+        it do
+          expected_content = File.read(File.join(File.dirname(__FILE__), 'files',
+            "runpuppet_without_wait_for_cert#{init_sys_suffix}"))
+          is_expected.to create_file('/var/www/ks/runpuppet').with_content(expected_content)
         end
-
-        context 'specify_ntp_servers_hash' do
-          let (:params) {{
-            :ntp_servers => {
-              '1.2.3.4' => ['foo, bar'],
-              '5.6.7.8' => ['baz']
-            }
-          }}
-
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_file('/var/www/ks/runpuppet').with_content(/ntpdate -b 1.2.3.4 5.6.7.8/) }
-        end
-
-        context 'no_print_stats' do
-          let(:params) {{
-            :runpuppet_print_stats => false
-          }}
-
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.not_to create_file('/var/www/ks/runpuppet').with_content(/--evaltrace/) }
-        end
-
-        context 'no_wait_for_cert' do
-          let(:params) {{
-            :runpuppet_wait_for_cert => false
-          }}
-
-          it { is_expected.to compile.with_all_deps }
-          it { is_expected.not_to create_file('/var/www/ks/runpuppet').with_content(/--waitforcert/) }
-        end
-
-
       end
     end
   end
