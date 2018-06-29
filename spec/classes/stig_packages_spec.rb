70bee8beb5b17be1c550fed8b55028cac2bed480
jeannegreulich <jeanne.greulich@onyxpoint.com>
2018-06-29 17:40:49 -0400

    fixed test

--- spec/classes/stig_packages_spec.rb
+++ spec/classes/stig_packages_spec.rb
@@ -11,7 +11,7 @@ describe 'simp::stig_packages' do
 
         context 'with default parameters' do
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_stig__packages('stig_packages') }
+          it { is_expected.to create_stig_packages('stig_packages') }
         end
 
       end
