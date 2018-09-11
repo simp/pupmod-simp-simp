658a57d02ab3c48553f23cbe9ae95325ae9dc075
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2018-09-11 12:43:43 -0400

    (SIMP-2791) Update to Hiera 5 (#168)
    - Update to Hiera 5
    - Go back to using saz/timezone versions that support Hiera 5
    - Use latest trlinkin/nsswitch
    
    SIMP-2791 #comment pupmod-simp-simp
    SIMP-4887 #comment pupmod-simp-simp
    SIMP-5331 #comment pupmod-simp-simp

--- spec/classes/nsswitch_spec.rb
+++ spec/classes/nsswitch_spec.rb
@@ -2,38 +2,16 @@ require 'spec_helper'
 
 describe 'simp::nsswitch' do
   context 'supported operating systems' do
-    on_supported_os.each do |os, facts|
+    on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) do
-          facts
+          os_facts
         end
 
-        context 'with default parameters on EL 6' do
+        context 'with default parameters' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_class('simp::nsswitch') }
-          if facts[:os][:release][:major] == 6 then
-            it { is_expected.to create_file('/etc/nsswitch.conf').with_content(<<-EOM) }
-# This file is controlled by Puppet
-
-passwd:     files
-shadow:     files
-group:      files
-sudoers:    files
-hosts:      files myhostname dns
-bootparams: nisplus [NOTFOUND=return] files
-ethers:     files
-netmasks:   files
-networks:   files
-protocols:  files
-rpc:        files
-services:   files
-netgroup:   files
-publickey:  nisplus
-automount:  files nisplus
-aliases:    files nisplus
-EOM
-          else
-            it { is_expected.to create_file('/etc/nsswitch.conf').with_content(<<-EOM) }
+          it { is_expected.to create_file('nsswitch.conf').with_content(<<-EOM) }
 # This file is controlled by Puppet
 
 passwd:     files
@@ -53,34 +31,11 @@ publickey:  nisplus
 automount:  files nisplus
 aliases:    files nisplus
 EOM
-          end
         end
 
         context 'with sssd => true' do
           let(:params) {{ :sssd => true }}
-          if facts[:os][:release][:major] == 6 then
-            it { is_expected.to create_file('/etc/nsswitch.conf').with_content(<<-EOM) }
-# This file is controlled by Puppet
-
-passwd:     files [!NOTFOUND=return] sss
-shadow:     files [!NOTFOUND=return] sss
-group:      files [!NOTFOUND=return] sss
-sudoers:    files [!NOTFOUND=return] sss
-hosts:      files myhostname dns
-bootparams: nisplus [NOTFOUND=return] files
-ethers:     files
-netmasks:   files
-networks:   files
-protocols:  files
-rpc:        files
-services:   files
-netgroup:   files [!NOTFOUND=return] sss
-publickey:  nisplus
-automount:  files nisplus
-aliases:    files nisplus
-EOM
-          else
-            it { is_expected.to create_file('/etc/nsswitch.conf').with_content(<<-EOM) }
+          it { is_expected.to create_file('nsswitch.conf').with_content(<<-EOM) }
 # This file is controlled by Puppet
 
 passwd:     files [!NOTFOUND=return] sss
@@ -100,34 +55,11 @@ publickey:  nisplus
 automount:  files nisplus
 aliases:    files nisplus
 EOM
-          end
         end
 
         context 'with ldap => true' do
           let(:params) {{ :ldap => true }}
-          if facts[:os][:release][:major] == 6 then
-            it { is_expected.to create_file('/etc/nsswitch.conf').with_content(<<-EOM) }
-# This file is controlled by Puppet
-
-passwd:     files [!NOTFOUND=return] ldap
-shadow:     files [!NOTFOUND=return] ldap
-group:      files [!NOTFOUND=return] ldap
-sudoers:    files [!NOTFOUND=return] ldap
-hosts:      files myhostname dns
-bootparams: nisplus [NOTFOUND=return] files
-ethers:     files
-netmasks:   files
-networks:   files
-protocols:  files
-rpc:        files
-services:   files
-netgroup:   files [!NOTFOUND=return] ldap
-publickey:  nisplus
-automount:  files nisplus
-aliases:    files nisplus
-EOM
-          else
-            it { is_expected.to create_file('/etc/nsswitch.conf').with_content(<<-EOM) }
+          it { is_expected.to create_file('nsswitch.conf').with_content(<<-EOM) }
 # This file is controlled by Puppet
 
 passwd:     files [!NOTFOUND=return] ldap
@@ -147,7 +79,6 @@ publickey:  nisplus
 automount:  files nisplus
 aliases:    files nisplus
 EOM
-          end
         end
 
       end
