b2c389adc01e6b649a014031335b7bf7ef1dc20f
Adam Yohrling <adam.yohrling@onyxpoint.com>
2019-01-03 14:40:53 -0500

    (SIMP-5919) Normalize Passwords
    This changes the code to hash cleartext passwords before dropping
    them into the system and to throw an error when insecure password
    hashes are used.

--- manifests/root_user.pp
+++ manifests/root_user.pp
@@ -14,10 +14,10 @@
 #  Set the root user's password using Puppet
 #
 class simp::root_user (
-  Boolean           $manage_perms = true,
-  Boolean           $manage_user  = true,
-  Boolean           $manage_group = true,
-  Optional[String]  $password     = undef,
+  Boolean                                         $manage_perms = true,
+  Boolean                                         $manage_user  = true,
+  Boolean                                         $manage_group = true,
+  Variant[Undef, String, Pattern[/^\$\d[ay]?\$/]] $password     = undef,
 ){
 
   simplib::assert_metadata( $module_name )
@@ -32,10 +32,14 @@ class simp::root_user (
   }
 
   if $manage_user {
-    # use Sensitive data type for the password if set
-    $l_password = $password ? {
-      undef   => undef,
-      default => Sensitive($password),
+    case $password {
+      undef:                { $_password = undef }
+      /^\$6\$/:             { $_password = Sensitive($password) }
+      /^\$[125]{1}[ay]?\$/: { fail('Error: You cannot use MD5, Blowfish, or SHA256 hashing algorithms for the user password. Please hash with SHA512.') }
+      default:              {
+        $_salt     = fqdn_rand_string(16)
+        $_password = Sensitive(pw_hash($password, 'SHA-512', $_salt))
+      }
     }
 
     user { 'root':
@@ -47,7 +51,7 @@ class simp::root_user (
       shell      => '/bin/bash',
       membership => 'minimum',
       forcelocal => true,
-      password   => $l_password,
+      password   => $_password,
     }
   }
 
