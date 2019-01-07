255c9eee368be6166036c63ae92eccdabebcf5f2
Adam Yohrling <ayohrling@users.noreply.github.com>
2019-01-07 12:26:08 -0500

    (SIMP-5919) Manage Root User Password (#182)
    This change adds the ability to set most `root` user attributes for the widest range of adoption.
    
    SIMP-5919 #close

--- manifests/root_user.pp
+++ manifests/root_user.pp
@@ -1,53 +1,99 @@
-# Manage resources related to the root user
+# Manage resources related to the `root` user
 #
 # @param manage_perms
-#   Ensure that /root has restricted permissions and proper SELinux
-#   contexts.
+#   Ensure that `$home` has restricted permissions and proper SELinux contexts.
 #
 # @param manage_user
-#   Ensure the root user has appropriate UIDs and groups, etc
+#   Ensure the `root` user has appropriate UIDs and groups, etc
 #
 # @param manage_group
-#  Ensure the root group has appropriate UIDs, etc
+#  Ensure the `root` group has appropriate UIDs, etc
+#
+# @param hashed_password
+#   Validate the correctness of the password hash and then pass it through to
+#   the `User` resource for `root`
+#
+# @param password
+#  Pass this through untouched to the `User` resource for `root`
+#
+#  * Please use `$hashed_password` if possible
+#
+# @param username
+#   The username of the `root` user
+#
+# @param uid
+#   The UID of the `root` user
+#
+# @param gid
+#   The GID of the `root` user
+#
+# @param shell
+#   The shell to use for the `root` user
+#
+# @param home
+#   The home directory of the `root` user
 #
 class simp::root_user (
-  Boolean $manage_perms = true,
-  Boolean $manage_user  = true,
-  Boolean $manage_group = true
+  Boolean                       $manage_perms    = true,
+  Boolean                       $manage_user     = true,
+  Boolean                       $manage_group    = true,
+  Optional[Simplib::ShadowPass] $hashed_password = undef,
+  Optional[String[1]]           $password        = undef,
+  String[1]                     $username        = 'root',
+  Integer[0]                    $uid             = 0,
+  Integer[0]                    $gid             = 0,
+  Stdlib::Absolutepath          $shell           = '/bin/bash',
+  Stdlib::Absolutepath          $home            = "/${username}"
 ){
 
   simplib::assert_metadata( $module_name )
 
   if $manage_perms {
-    file { '/root':
+    file { $home:
       ensure => 'directory',
-      owner  => 'root',
-      group  => 'root',
+      owner  => $username,
+      group  => $username,
       mode   => '0550'
     }
   }
 
   if $manage_user {
-    user { 'root':
+
+    if $password and $hashed_password {
+      fail('Error: You cannot specify both "$password" and "$hashed_password"')
+    }
+
+    if $password {
+      $_password = $password
+    }
+    elsif $hashed_password {
+      $_password = $hashed_password
+    }
+    else {
+      $_password = undef
+    }
+
+    user { $username:
       ensure     => 'present',
-      uid        => '0',
-      gid        => '0',
+      uid        => $uid,
+      gid        => $gid,
       allowdupe  => false,
-      home       => '/root',
-      shell      => '/bin/bash',
+      home       => $home,
+      shell      => $shell,
       membership => 'minimum',
-      forcelocal => true
+      forcelocal => true,
+      password   => Sensitive($_password)
     }
   }
 
   if $manage_group {
-    group { 'root':
+    group { $username:
       ensure          => 'present',
-      gid             => '0',
+      gid             => $gid,
       allowdupe       => false,
       auth_membership => true,
       forcelocal      => true,
-      members         => ['root']
+      members         => [$username]
     }
   }
 }
--- spec/classes/root_user_spec.rb
+++ spec/classes/root_user_spec.rb
@@ -8,10 +8,10 @@ describe 'simp::root_user' do
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to create_class('simp::root_user') }
 
-      it 'manages root user by default' do
-        is_expected.to create_file('/root')
-        is_expected.to create_user('root')
-        is_expected.to create_group('root')
+      context 'manages root user by default' do
+        it { is_expected.to create_file('/root') }
+        it { is_expected.to create_user('root').without_password }
+        it { is_expected.to create_group('root') }
       end
 
       context 'unless told not to' do
@@ -20,13 +20,36 @@ describe 'simp::root_user' do
           :manage_user  => false,
           :manage_group => false
         }}
-        it {
-          is_expected.not_to create_file('/root')
-          is_expected.not_to create_user('root')
-          is_expected.not_to create_group('root')
-        }
+        it { is_expected.not_to create_file('/root') }
+        it { is_expected.not_to create_user('root') }
+        it { is_expected.not_to create_group('root') }
       end
 
+      context 'with clear-text password' do
+        let(:params) {{
+          :password => 'mysecretpassword'
+        }}
+        it { is_expected.to create_file('/root') }
+        it { is_expected.to create_user('root').with_password('mysecretpassword') }
+        it { is_expected.to create_group('root') }
+      end
+
+      context 'with sha512 password' do
+        let(:params) {{
+          :hashed_password => '$6$fdkjfdk$yj8HAo/RyW/WhYkXvTp7nQbjIZz4TMRuj/0W1bJGuQjGxea36JhUkB36BMyf8O/g0/rpRB1lPC/6KuAmgqnIn0'
+        }}
+        it { is_expected.to create_file('/root') }
+        it { is_expected.to create_user('root').with_password('$6$fdkjfdk$yj8HAo/RyW/WhYkXvTp7nQbjIZz4TMRuj/0W1bJGuQjGxea36JhUkB36BMyf8O/g0/rpRB1lPC/6KuAmgqnIn0') }
+        it { is_expected.to create_group('root') }
+      end
+
+      context 'with both password and hashed_password specified' do
+        let(:params) {{
+          :password        => 'mysecretpassword',
+          :hashed_password => '$6$fdkjfdk$yj8HAo/RyW/WhYkXvTp7nQbjIZz4TMRuj/0W1bJGuQjGxea36JhUkB36BMyf8O/g0/rpRB1lPC/6KuAmgqnIn0'
+        }}
+        it { is_expected.to compile.and_raise_error(/Error: You cannot specify both "\$password" and "\$hashed_password"/) }
+      end
     end
   end
 end
