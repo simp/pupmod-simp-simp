257d3be0e86ff2df9268ea563bbe18ef50f233c2
lnemsick-simp <lnemsick-simp@users.noreply.github.com>
2019-03-08 14:34:17 -0500

    (SIMP-6217) Update to 7.1.0 puppetdb module (#194)
    - Deprecated simp::puppetdb::read_database_ssl.  Use
      simp::puppetdb::read_database_jdbc_ssl_properties which maps
      directly to puppetdb::server::read_database_jdbc_ssl_properties
      (puppetdb version >= 7.0.0).
    - Updated to a minimum puppetdb module version 7.1.0 in the
      metadata.json and expanded the upper bound accordingly
    - Expanded the upper bound for the concat and stdlib Puppet modules
      in the metadata.json
    - Updated URLs in the README.md
    
    SIMP-6217 #comment pupmod-simp-simp

--- spec/classes/puppetdb_spec.rb
+++ spec/classes/puppetdb_spec.rb
@@ -7,12 +7,54 @@ describe 'simp::puppetdb' do
         let(:facts) do
           { :puppet_settings => { 'main' => { 'hostprivkey' => 'blah' } }}.merge(os_facts)
         end
+
         context 'with default parameters' do
           let(:hieradata) { 'simp__puppetdb' }
-          # Overriding the file definition for puppetdb::ssl_cert/key/ca to use
-          # undef for content results in:
-          # Could not understand source file://: bad URI(absolute but no path)
-          pending('is_expected.to compile.with_all_deps')
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('simp::puppetdb') }
+          it { is_expected.to contain_class('puppetdb').with(
+            :listen_address                    => '127.0.0.1',
+            :listen_port                       => 8138,
+            :open_listen_port                  => false,
+            :ssl_deploy_certs                  => true,
+            :ssl_set_cert_paths                => true,
+            :ssl_listen_address                => '0.0.0.0',
+            :ssl_listen_port                   => 8139,
+            :disable_ssl                       => false,
+            :manage_package_repo               => false,
+#            :database_password      => varies from run-to-run
+            :read_database_username            => 'simp_puppetdb',
+#            :read_database_password => varies from run-to-run
+            :read_database_name                => 'simp_puppetdb',
+            :read_database_jdbc_ssl_properties => '?ssl=true',
+            :manage_firewall                   => false,
+#            :java_args              => Xmx & Xms vary because of OS memory differences in facts
+          ) }
+          it { is_expected.to contain_class('puppetdb::master::config') }
+          it {
+            is_expected.to contain_file("#{Puppet[:confdir]}/puppetdb.conf").with( {
+              :owner => 'root',
+              :group => 'root',
+              :mode  => '0644'
+            } )
+          }
+        end
+
+        context 'with read_database_ssl = true' do
+          let(:hieradata) { 'simp__puppetdb' }
+          let(:params) {{ :read_database_ssl => true }}
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('puppetdb').with_read_database_jdbc_ssl_properties('?ssl=true') }
+        end
+
+        context 'with read_database_ssl = false' do
+          let(:hieradata) { 'simp__puppetdb' }
+          let(:params) {{ :read_database_ssl => false }}
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_class('puppetdb').with_read_database_jdbc_ssl_properties('') }
         end
 
         context 'with use_puppet_ssl_certs => false' do
