5953e4c417064cd4c3033e951eb79f85b241f2b2
sharkbruhaha <44031572+sharkbruhaha@users.noreply.github.com>
2019-04-12 12:04:09 -0400

    fixed indention

--- functions/yum/repo/sanitize_simp_release_slug.pp
+++ functions/yum/repo/sanitize_simp_release_slug.pp
@@ -8,23 +8,22 @@
 function simp::yum::repo::sanitize_simp_release_slug(
   Variant[String,Undef] $simp_release_slug = undef
 ) {
+    inspect(simplib::simp_version())
 
-  inspect(simplib::simp_version())
-
-  if defined('$simp_release_slug') and !empty($simp_release_slug) {
-    $_release_slug = $simp_release_slug
-  }
-  else {
-    $simp_version = simplib::simp_version()
-    $_simp_maj_version = (split($simp_version,'\.'))[0]
-
-    if $_simp_maj_version in ['6', '5'] {
-      $_release_slug = "${_simp_maj_version}_X"
+    if defined('$simp_release_slug') and !empty($simp_release_slug) {
+      $_release_slug = $simp_release_slug
     }
     else {
-      fail("SIMP version ${simp_version} does not map to a known yum repository slug")
+      $simp_version = simplib::simp_version()
+      $_simp_maj_version = (split($simp_version,'\.'))[0]
+
+      if $_simp_maj_version in ['6', '5'] {
+        $_release_slug = "${_simp_maj_version}_X"
+      }
+      else {
+        fail("SIMP version ${simp_version} does not map to a known yum repository slug")
+      }
     }
+    err "XXX release_slug [${_release_slug}]"
+    $_release_slug
   }
-  err "XXX release_slug [${_release_slug}]"
-  $_release_slug
-}
