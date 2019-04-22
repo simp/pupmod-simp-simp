ca219a3f3bda4e80065dcd68169687f5ea1426c2
Trevor Vaughan <tvaughan@onyxpoint.com>
2019-04-22 17:11:00 -0400

    (SIMP-4663) Use systemd for tmp.mounts if possible (#204)
    * (SIMP-4663) Use systemd for tmp.mounts if possible
    - Refactor the simp::mountpoints::tmp to use systemd's tmp.mount target if
      the system supports systemd.
    - Fix tmp permissions
    - handle case where not securing but want tmp.mounts
    - fix top level mountpoints test
    - Ensure proper service ordering
    - Fix service-related test
    - fixed ruby version
    SIMP-4663 #close

--- spec/classes/mountpoints_spec.rb
+++ spec/classes/mountpoints_spec.rb
@@ -2,30 +2,36 @@ require 'spec_helper'
 
 describe 'simp::mountpoints' do
   context 'supported operating systems' do
-    on_supported_os.each do |os, facts|
+    on_supported_os.each do |os, os_facts|
       context "on #{os}" do
         let(:facts) do
-          # facts[:selinux_mode] = 'disabled'
-          facts
+          os_facts
         end
 
         context 'with default parameters' do
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_mount('/dev/pts').with_options('rw,gid=5,mode=620,noexec') }
           it { is_expected.to contain_mount('/sys').with_options('rw,nodev,noexec') }
-          it { is_expected.to contain_file('/tmp').that_comes_before('Mount[/tmp]') }
+          if os_facts[:init_systems].include?('systemd')
+            it { is_expected.to contain_systemd__unit_file('tmp.mount') }
+          else
+            it { is_expected.to contain_file('/tmp').that_comes_before('Mount[/tmp]') }
+          end
           it { is_expected.to contain_file('/var/tmp').that_comes_before('Mount[/var/tmp]') }
         end
 
         context 'and manage_tmp_perms => false' do
           let(:params) {{ :manage_tmp_perms => false }}
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.not_to contain_mount('/tmp') }
+          if os_facts[:init_systems].include?('systemd')
+            it { is_expected.not_to contain_systemd__unit_file('tmp.mount') }
+          else
+            it { is_expected.not_to contain_mount('/tmp') }
+          end
           it { is_expected.not_to contain_mount('/var/tmp') }
           it { is_expected.not_to contain_file('/tmp') }
           it { is_expected.not_to contain_file('/var/tmp') }
         end
-
       end
     end
   end
