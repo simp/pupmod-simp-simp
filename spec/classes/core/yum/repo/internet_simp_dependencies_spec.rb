ae7631982e78b226b7449b8fa83595404be84755
Trevor Vaughan <tvaughan@onyxpoint.com>
2019-07-07 16:49:58 -0400

    split classes to prevent timeout

--- spec/classes/core/yum/repo/internet_simp_dependencies_spec.rb
+++ spec/classes/core/yum/repo/internet_simp_dependencies_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
-metadata_file = File.expand_path(File.join(__dir__, '..', '..', '..', '..', 'metadata.json'))
+metadata_file = File.expand_path(File.join(__dir__, '..', '..', '..', '..', '..', 'metadata.json'))
 metadata_json = File.read(metadata_file, {:encoding => "utf-8"} )
 
-describe 'simp::yum::repo::internet_simp_server' do
+describe 'simp::yum::repo::internet_simp_dependencies' do
+
   on_supported_os.each do |os, os_facts|
     before(:each) do
       Puppet::Parser::Functions.newfunction(:load_module_metadata, :type => :rvalue) { |args| JSON.load(metadata_json) }
     end
 
     context "on #{os}" do
+
       let(:facts) do
         os_facts
       end
@@ -18,7 +20,7 @@ describe 'simp::yum::repo::internet_simp_server' do
         let(:params) {{ :simp_release_slug => '5_X' }}
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to contain_yumrepo('simp-project_5_X') }
+        it { is_expected.to contain_yumrepo('simp-project_5_X_Dependencies') }
       end
 
       context 'when `simp_release_slug` is undef' do
@@ -29,6 +31,7 @@ describe 'simp::yum::repo::internet_simp_server' do
             let(:pre_condition) do
               "function simplib::simp_version() { '#{_version}' }"
             end
+
             it do
               is_expected.to raise_error(/SIMP/)
             end
@@ -42,11 +45,13 @@ describe 'simp::yum::repo::internet_simp_server' do
             let(:pre_condition) do
               "function simplib::simp_version() { '#{_version}' }"
             end
+
             it do
               is_expected.to compile.with_all_deps
             end
           end
         end
+
       end
     end
   end
