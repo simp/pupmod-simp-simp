1252267ed98115456190d9377415ad1bf42f62b2
jeannegreulich <jeanne.greulich@onyxpoint.com>
2020-09-28 08:43:27 -0400

    (SIMP-7569) SSSD client setup changes (#239)
    - Change the setup for the SSSD client to match changes
      in the SSSD module for SIMP.
      - use file id_provider for el7 and later in LOCAL sssd domain.
      - no longer configure other services because
        it is done with sssd parameters.
    
    SIMP-7569 #close

---
+++ spec/classes/01_classes/sssd/client_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+shared_examples_for 'sssd client' do
+  it { is_expected.to compile.with_all_deps }
+  it { is_expected.to contain_class('sssd') }
+end
+
+describe 'simp::sssd::client' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) { os_facts }
+
+        if os_facts[:kernel] == 'windows'
+          it { expect{ is_expected.to compile.with_all_deps }.to raise_error(/'windows .+' is not supported/) }
+        else
+          context 'with default parameters' do
+            it_should_behave_like 'sssd client'
+            if os_facts[:os][:release][:major] <= '7'
+              it { is_expected.to contain_sssd__domain('LOCAL')}
+            else
+              it { is_expected.to_not contain_sssd__domain('LOCAL')}
+            end
+            it { is_expected.to_not contain_sssd__domain('LDAP')}
+          end
+
+          context 'with alternate params' do
+            let(:params) {{
+              :ldap_domain       => true,
+              :local_domain      => true,
+              :autofs            => false,
+              :sudo              => false,
+              :ssh               => false,
+              :enumerate_users   => true,
+              :cache_credentials => false,
+              :min_id            => 501
+            }}
+            it_should_behave_like 'sssd client'
+            if os_facts[:os][:release][:major] <= '6'
+              it { is_expected.to contain_sssd__domain('LOCAL').with({
+                'id_provider'   => 'local',
+                'auth_provider' => 'local',
+                'min_id' => 501,
+                'enumerate' => false,
+                'cache_credentials' => false
+                })
+              }
+            else
+              it { is_expected.to contain_sssd__domain('LOCAL').with({
+                'id_provider'   => 'files',
+                'min_id' => 501,
+                'enumerate' => false,
+                'cache_credentials' => false
+                })
+              }
+            end
+            it { is_expected.to contain_sssd__domain('LDAP').with({
+              'min_id' => 501,
+              'enumerate' => true,
+              'cache_credentials' => false
+              })
+            }
+          end
+        end
+      end
+    end
+  end
+end
