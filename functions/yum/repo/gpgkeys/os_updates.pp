8245d5cb0caeef7928b56af64b4f7abc5dd98892
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2021-10-18 17:30:55 -0400

    Use Centos-<ver> for centos keys.

--- functions/yum/repo/gpgkeys/os_updates.pp
+++ functions/yum/repo/gpgkeys/os_updates.pp
@@ -5,12 +5,7 @@ function simp::yum::repo::gpgkeys::os_updates() {
   case $facts['os']['name'] {
     'RedHat': {$release_key = 'RPM-GPG-KEY-redhat-release'}
     'OracleLinux': {$release_key = 'RPM-GPG-KEY-oracle'}
-    'CentOS': {
-      case $facts['os']['release']['major'] {
-        '7','8': {  $release_key = "RPM-GPG-KEY-${facts['os']['name']}-${facts['os']['release']['major']}"}
-        default: { $release_key = "RPM-GPG-KEY-${facts['os']['name']}-Official"}
-      }
-    }
+    'CentOS': {$release_key = "RPM-GPG-KEY-${facts['os']['name']}-${facts['os']['release']['major']}"}
     default: { $release_key = "RPM-GPG-KEY-${facts['os']['name']}" }
   }
   [$release_key]
