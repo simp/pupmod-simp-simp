9bec821b0e60718ac8b4fb038d43faff5b1a59a3
Steven Pritchard <steve@sicura.us>
2024-06-23 09:09:26 -0500

    Switch from mocha to rspec mocks (#319)
    * Switch from mocha to rspec mocks
    
    * Temporarily pin to facterdb < 2 to allow legacy facts
    
    * More mocha -> rspec conversion

--- spec/facter/simp_rsync_environments_spec.rb
+++ spec/facter/simp_rsync_environments_spec.rb
@@ -9,18 +9,18 @@ describe "SIMP Rsync Environments" do
 
   context "if #{rsync_env_dir} present" do
     before :each do
-      File.stubs(:directory?).at_least_once.returns(true)
+      allow(File).to receive(:directory?).at_least(:once).and_return(true)
 
       # All the random that rspec itself does
-      Dir.expects(:chdir).at_least_once
+      allow(Dir).to receive(:chdir).at_least(:once)
 
-      Dir.expects(:chdir).with(rsync_env_dir).at_least_once.returns(true)
+      allow(Dir).to receive(:chdir).with(rsync_env_dir).at_least(:once).and_return(true)
 
-      Dir.expects(:glob).with('*/rsync').at_least_once.returns(['simp/rsync'])
+      allow(Dir).to receive(:glob).with('*/rsync').at_least(:once).and_return(['simp/rsync'])
 
-      Facter::Core::Execution.expects(:exec).with("find -L simp -name '.shares'").at_least_once.returns("simp/rsync/.shares")
+      allow(Facter::Core::Execution).to receive(:exec).with("find -L simp -name '.shares'").at_least(:once).and_return("simp/rsync/.shares")
 
-      Dir.expects(:glob).with('simp/rsync/*').at_least_once.returns([
+      allow(Dir).to receive(:glob).with('simp/rsync/*').at_least(:once).and_return([
         'simp/rsync/Global/mcafee',
         'simp/rsync/Global/clamav'
       ])
@@ -49,7 +49,7 @@ describe "SIMP Rsync Environments" do
 
   context "if #{rsync_env_dir} absent" do
     before :each do
-      File.stubs(:directory?).returns(false)
+      allow(File).to receive(:directory?).and_return(false)
 
       LegacyFacter.collection.load(:simp_rsync_environments)
     end
@@ -61,12 +61,12 @@ describe "SIMP Rsync Environments" do
 
   context "if #{rsync_env_dir} empty" do
     before :each do
-      File.stubs(:directory?).returns(true)
+      allow(File).to receive(:directory?).and_return(true)
 
-      Dir.expects(:chdir).at_least_once
-      Dir.expects(:glob).with('*/rsync').at_least_once.returns(['simp/rsync'])
+      allow(Dir).to receive(:chdir).at_least(:once)
+      allow(Dir).to receive(:glob).with('*/rsync').at_least(:once).and_return(['simp/rsync'])
 
-      Facter::Core::Execution.expects(:exec).with("find -L simp -name '.shares'").at_least_once.returns('')
+      allow(Facter::Core::Execution).to receive(:exec).with("find -L simp -name '.shares'").at_least(:once).and_return('')
 
       LegacyFacter.collection.load(:simp_rsync_environments)
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,7 +98,7 @@ RSpec.configure do |c|
   c.trusted_server_facts = true if c.respond_to?(:trusted_server_facts)
 
   c.mock_framework = :rspec
-  c.mock_with :mocha
+  c.mock_with :rspec
 
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests') if c.respond_to?(:manifest_dir)
